1018699a6cf003918edcb20c2e07b4cda74e5d49
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-10 04:55:05 +0900

    feat: 당첨 로또 입력받는 메소드 따로 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.ExceptionString;
 
 public class InputView {
     public int inputMoney() {
@@ -8,15 +9,25 @@ public class InputView {
             String input = Console.readLine();
             int result = Integer.parseInt(input);
             if (result % 1000 != 0)
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(ExceptionString.NOTDIVIDE.getPrint());
             return result;
         } catch (Exception e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ExceptionString.NOTNUMBER.getPrint());
         }
+    }
+
+    public String inputWinningNumbers() {
+        String input = Console.readLine();
+        return input;
     }
 
     public String inputNumber() {
         String input = Console.readLine();
+        try {
+            Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(ExceptionString.NOTNUMBER.getPrint());
+        }
         return input;
     }
 }
