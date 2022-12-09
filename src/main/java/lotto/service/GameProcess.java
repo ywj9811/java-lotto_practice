bafb14e8978cc885fc3f4e3d6a5eaff648f0e8a1
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-10 03:59:41 +0900

    feat: 게임 총 진행 구현

---
+++ src/main/java/lotto/service/GameProcess.java
@@ -0,0 +1,31 @@
+package lotto.service;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
+public class GameProcess {
+    private MakeWinningNumber makeWinningNumber = new MakeWinningNumber();
+    private MakeBuyLottoNumbers makeBuyLottoNumbers = new MakeBuyLottoNumbers();
+    private WinningCheck winningCheck = new WinningCheck();
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+    private int[] winningCount;
+
+    public void gameStart() {
+        outputView.buyMoney();
+        int buyMoney = inputView.inputMoney();
+        outputView.howMany(buyMoney);
+        List<List<Integer>> buyLottos = makeBuyLottoNumbers.buyLotto(buyMoney);
+        outputView.myLottos(buyLottos);
+        outputView.winningNumber();
+        List<Integer> winningNumber = makeWinningNumber.getWinningNumber(inputView.inputNumber());
+        outputView.bonusNumber();
+        int bonusNumber = makeWinningNumber.makeBonusNumber(inputView.inputNumber());
+        winningCount = winningCheck.winningCheck(buyLottos, winningNumber, bonusNumber);
+        int getMoney = winningCheck.getMoney(winningCount);
+        double returnOfMoney = winningCheck.returnOfMoney(buyMoney, getMoney);
+        outputView.getReturn(returnOfMoney);
+    }
+}
