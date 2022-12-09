76813a187de37cbf52cc26962224c9c96e2c5268
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-10 04:12:25 +0900

    fix: 출력문 위한 수정

--- src/main/java/lotto/domain/WinningMoney.java
+++ src/main/java/lotto/domain/WinningMoney.java
@@ -1,7 +1,7 @@
 package lotto.domain;
 
 public enum WinningMoney {
-    FIRST("6개 일치 (2,000,000,000원 - "),
+    FIRST("6개 일치 (2,000,000,000원) - "),
     SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
     THIRD("5개 일치 (1,500,000원) - "),
     FOURTH("4개 일치 (50,000원) - "),
