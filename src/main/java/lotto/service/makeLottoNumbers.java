eb558b778eeb1fb7af4cf32be8302b4ee0f31da2
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-10 01:02:45 +0900

    feat: 로또 구매시 구매한 금액에 맞춰 로또 번호 반환 메소드 생성

--- src/main/java/lotto/service/makeLottoNumbers.java
+++ src/main/java/lotto/service/makeLottoNumbers.java
@@ -12,6 +12,16 @@ public class makeLottoNumbers {
     private static final int MIN_NUM = 1;
     private static final int SIZE = 6;
 
+    public List<List<Integer>> buyLotto(int money) {
+        List<List<Integer>> myLottos = new ArrayList<>();
+        for (int i = 0; i < money/1000; i++) {
+            List<Integer> lottoNumber = makeNumbers();
+            Collections.sort(lottoNumber);
+            myLottos.add(lottoNumber);
+        }
+        return myLottos;
+    }
+
     public List<Integer> makeNumbers() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUM, MAX_NUM, SIZE);
         Lotto lotto = new Lotto(lottoNumbers);
